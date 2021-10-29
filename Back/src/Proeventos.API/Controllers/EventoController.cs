9b6a0f7f90255eba1f474d2b036367048f961e42
Gustavo Gaspar <gustavominghini@gmail.com>
2021-10-29 13:16:35 -0300

    first commit

---
+++ Back/src/Proeventos.API/Controllers/EventoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Proeventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+    
+
+
+
+        public EventoController(ILogger<EventoController> logger)
+        {
+           
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Value";
+        }
+        
+        [HttpPost]
+        public string Post()
+        {
+            return "cuzao do caramba";
+        }
+
+         [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"agora tem o metodo put id= {id}";
+        }
+
+        [HttpPut("{id}")]
+        public string Delete(int id)
+        {
+            return $"agora tem o Delete put id= {id}";
+        }
+    }
+}
